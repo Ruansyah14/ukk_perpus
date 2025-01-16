d2248e469a8c32e2d01113c40cf99ee058cbc4e5
Ruansyah14 <achmadlight@gmail.com>
2025-01-16 15:55:25 +0700

    skema database clear

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->text('addres');
+            $table->string('status')->default('member');
             $table->rememberToken();
             $table->timestamps();
         });
