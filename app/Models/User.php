2e6f14470e27717c79e6ce9bd38210dc2710801e
Ruansyah14 <achmadlight@gmail.com>
2025-01-20 14:32:37 +0700

    relationship clear

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Pinjam;
+use App\Models\Ulasan;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,4 +43,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function pinjam()
+    {
+        return $this->hasMany(Pinjam::class);
+    }
+    public function ulasan()
+    {
+        return $this->hasMany(Ulasan::class);
+    }
 }
